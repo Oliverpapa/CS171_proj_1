40d796e31721b9de972c98028563f367b1524595
Erin Luo <haolinl4@uci.edu>
2019-11-09 21:16:27 -0800

    heuristic and simple algorithm

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -21,21 +21,38 @@ class StudentAI():
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
 
-        move = select_move(moves)
-        #index = randint(0,len(moves)-1)
-        #inner_index =  randint(0,len(moves[index])-1)
-        #move = moves[index][inner_index]
-
+        # index = randint(0, len(moves) - 1)
+        # inner_index = randint(0, len(moves[index]) - 1)
+        # move = moves[index][inner_index]
+        move = self.select_move(moves)
         self.board.make_move(move,self.color)
         return move
 
-    def select_move(moves):
+    def select_move(self,moves):
         """take a list of moves and select the best one,
         return move"""
-        return moves[0]
+        best = [0, -99]     # store the best move in a list with its heuristic value
+        for checker in moves:
+            for move in checker:
+                if self.heuristic_func(move) > best[1]:
+                    best[0] = move
+                    best[1] = self.heuristic_func(move)
+        print (type(best[0]))
+        return best[0]
+
 
     def heuristic_func(self, move):
         """take a move, evaluate it's heuristic value
         return the value"""
-        return 0
+        result = 0
+        self.board.make_move(move,self.color)
+        print (self.board.board)
+        for row in self.board.board:
+            for checker in row:
+                if checker.get_color == self.color:
+                    x = checker.get_location()[0]
+                    y = checker.get_location()[1]
+                    result += y
+        self.board.undo()
+        return result
 
